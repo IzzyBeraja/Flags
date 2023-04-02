515dfe84b40290518b111f91813e672a90202aba
Izzy Beraja <IzzyBeraja+github@gmail.com>
2023-04-02 01:57:58 -0400

    Added user registration with "some" error handling
    - Added user reqister endpoint
    - Did some mild error handling. Need to make it more robust as errors currently crash the server
    - Will also need to better manage passwords as they can easily be sent out

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,12 +1,52 @@
+import { BAD_REQUEST, CREATED } from "../errors/errorCodes";
+
+import { Prisma } from "@prisma/client";
+import bcrypt from "bcrypt";
 import express from "express";
 
 const router = express.Router();
+const saltRounds = 10;
 
-router.get("/getUser", async (_req, res) => {
-  const data = await _req.prisma.user.findMany({
+router.get("/", async (req, res) => {
+  const data = await req.prisma.user.findMany({
     include: { links: true },
   });
   res.json(data);
+});
+
+router.post("/register", async (req, res) => {
+  if (!req.body["email"] || !req.body["name"] || !req.body["password"]) {
+    return res
+      .status(BAD_REQUEST)
+      .send(
+        "To register a new user, a valid email, name, and password are required"
+      );
+  }
+
+  const hashedPassword = await bcrypt.hash(req.body["password"], saltRounds);
+
+  try {
+    const createdUser = await req.prisma.user.create({
+      data: {
+        email: req.body["email"].toLowerCase(),
+        name: req.body["name"],
+        password: hashedPassword,
+      },
+    });
+
+    return res.status(CREATED).json({ ...createdUser, password: undefined });
+  } catch (err) {
+    if (
+      err instanceof Prisma.PrismaClientKnownRequestError &&
+      err.code === "P2002"
+    ) {
+      return res
+        .status(BAD_REQUEST)
+        .send("A account with that email already exists");
+    }
+
+    return res.status(BAD_REQUEST).send("Something else went wrong");
+  }
 });
 
 export default router;
