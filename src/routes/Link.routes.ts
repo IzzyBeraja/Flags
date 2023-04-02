595d383a35db026eff03889ecad9b005e808a6da
Izzy Beraja <IzzyBeraja+github@gmail.com>
2023-04-01 23:29:02 -0400

    Updated Link routes to include update and delete
    - I also changed the routes for each of the links
    |From|To|
    |getPost|/|
    |createPost|/create|
    
    - I will also be taking a look at how to properly handle prisma errors alongside other errors in the future.

--- src/routes/Link.routes.ts
+++ src/routes/Link.routes.ts
@@ -1,30 +1,76 @@
-import { BAD_REQUEST } from "../errors/errorCodes";
+import { BAD_REQUEST, CREATED, NOT_FOUND, OK } from "../errors/errorCodes";
 
 import express from "express";
 
 const router = express.Router();
 
-router.get("/getPost", async (req, res) => {
+router.get("/", async (req, res) => {
   const data = await req.prisma.link.findMany({
     include: { createdBy: true },
   });
-  res.json(data);
+  return res.json(data);
 });
 
-router.post("/createPost", async (req, res) => {
+router.post("/create", async (req, res) => {
   if (!req.body["description"] || !req.body["url"]) {
-    res.status(BAD_REQUEST).send("Bad Request");
+    return res.status(BAD_REQUEST).send("Bad Request");
   }
 
-  // const response = await req.prisma.link.create({
-  //   data: {
-  //     createdBy: { connect: { id: "0df7ae7d-4f48-4e34-ba2a-705476c9e3ef" } },
-  //     description: req.body["description"],
-  //     url: req.body["url"],
-  //   },
-  //   include: { createdBy: true },
-  // });
-  res.json("HelloWorld");
+  const prisma = await req.prisma.link.create({
+    data: {
+      createdBy: { connect: { id: "0df7ae7d-4f48-4e34-ba2a-705476c9e3ef" } },
+      description: req.body["description"],
+      url: req.body["url"],
+    },
+  });
+  return res.status(CREATED).json(prisma);
+});
+
+router.post("/update", async (req, res) => {
+  if (!req.body["description"] && !req.body["url"]) {
+    return res
+      .status(BAD_REQUEST)
+      .send("Either a descrption or url must be provided to update a post");
+  }
+
+  if (!req.body["id"]) {
+    return res
+      .status(BAD_REQUEST)
+      .send("A valid id must be provided to update a post");
+  }
+
+  const prisma = await req.prisma.link.update({
+    data: {
+      description: req.body["description"] ?? undefined,
+      url: req.body["url"] ?? undefined,
+    },
+    where: {
+      id: req.body["id"],
+    },
+  });
+
+  return res.status(OK).json(prisma);
+});
+
+router.delete("/delete", async (req, res) => {
+  if (!req.body["id"]) {
+    return res
+      .status(BAD_REQUEST)
+      .send("A valid id must be provided to delete a post");
+  }
+
+  //> TODO - Handle all of the error codes with proper error messages
+  try {
+    const prisma = await req.prisma.link.delete({
+      where: { id: req.body["id"] },
+    });
+
+    return res.status(OK).json(prisma);
+  } catch (err) {
+    return res
+      .status(NOT_FOUND)
+      .send("The post with the id provided was not found");
+  }
 });
 
 export default router;
