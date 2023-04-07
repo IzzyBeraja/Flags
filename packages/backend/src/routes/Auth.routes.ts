b927b2488771799fed713f6a6cf10af5715bf464
Izzy Beraja <IzzyBeraja+github@gmail.com>
2023-04-07 16:03:18 -0400

    Moved the auth routes to a separate file
    After looking at other APIs like discord and slack, I've noticed that they
    tend to separate out the login, register, and logout routes into a separate
    route under Auth.
    
    This makes sense because it's not really a user route, that I can use to
    return or modify user data.

---
+++ packages/backend/src/routes/Auth.routes.ts
@@ -0,0 +1,75 @@
+import { sessionName } from "./../middleware/session.middleware";
+import { BAD_REQUEST, CREATED, OK } from "../errors/errorCodes";
+import { createUser, loginUser } from "../queries/User.queries";
+import { validate } from "../validation/validateRequest";
+
+import express from "express";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  validate([
+    body("email", "A valid email is required").isEmail(),
+    body("name", "A valid name is required").isAlphanumeric(),
+    body("password", "A valid password is required").isLength({ min: 6 }),
+  ]),
+  async (req, res) => {
+    const createUserRequest = await createUser(req.prisma, {
+      email: req.body["email"].toLowerCase(),
+      name: req.body["name"],
+      password: req.body["password"],
+    });
+
+    return createUserRequest.success
+      ? res.status(CREATED).json(createUserRequest.createdUser)
+      : res.status(BAD_REQUEST).send(createUserRequest.error);
+  }
+);
+
+router.post(
+  "/login",
+  validate([
+    body("email", "A valid email is required").isEmail(),
+    body("password", "A valid password is required").isLength({ min: 6 }),
+  ]),
+  async (req, res) => {
+    if (req.session.userId != null) {
+      return res.status(BAD_REQUEST).send("You are already logged in");
+    }
+
+    const getUserRequest = await loginUser(
+      req.prisma,
+      req.body["email"].toLowerCase(),
+      req.body["password"]
+    );
+
+    if (getUserRequest.success) {
+      req.session.userId = getUserRequest.user.id;
+      return res.status(OK).send("Login successful");
+    }
+
+    return res.status(BAD_REQUEST).send(getUserRequest.error);
+  }
+);
+
+router.post("/logout", (req, res) => {
+  if (req.session.userId == null) {
+    return res.status(BAD_REQUEST).send("You are not logged in");
+  }
+
+  //> TODO - Modify this to use async/await
+  req.session.destroy(err => {
+    if (err) {
+      console.error(err);
+      return res.status(BAD_REQUEST).send("Something went wrong");
+    }
+
+    res.clearCookie(sessionName);
+    return res.status(OK).send("Logout successful");
+  });
+  return;
+});
+
+export default router;
--- packages/backend/src/routes/router.ts
+++ packages/backend/src/routes/router.ts
@@ -1,9 +1,11 @@
+import AuthRoutes from "./Auth.routes";
 import LinkRoutes from "./Link.routes";
 import UserRoutes from "./User.routes";
 
 import express from "express";
 
 const router = express.Router();
+router.use("/auth", AuthRoutes);
 router.use("/user", UserRoutes);
 router.use("/link", LinkRoutes);
 
