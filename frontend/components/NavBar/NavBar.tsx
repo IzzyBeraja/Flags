7e9115c586d3cbdf1fdce7d339613b34a4ce5fea
IzzyBeraja <izzyberaja+github@gmail.com>
2023-06-14 15:47:21 -0700

    Changed from NavBarItem to NavLink from Mantine
    I didn't realize that there was an already working version of the
    NavBarItem within the Mantine library so I decided to switch to it.
    
    I also now keep track of the active page using the next/router useRouter
    hook so the active tab is now shown in the navbar.

--- frontend/components/NavBar/NavBar.tsx
+++ frontend/components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
-import NavBarItem from "@components/NavBarItem/NavBarItem";
-import { Navbar as M_NavBar } from "@mantine/core";
+import { Navbar as M_NavBar, NavLink, useMantineTheme } from "@mantine/core";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import {
   Bug,
   CircuitSwitchOpen,
@@ -14,46 +15,52 @@ const navItems = [
     href: "/dashboard",
     icon: LayoutDashboard,
     label: "Dashboard",
-    tooltip: "Dashboard",
   },
   {
     href: "/analytics",
     icon: ReportAnalytics,
     label: "Analytics",
-    tooltip: "Analytics",
   },
   {
     href: "/flags",
     icon: Flag,
     label: "Flags",
-    tooltip: "Flags",
   },
   {
     href: "/killswitches",
     icon: CircuitSwitchOpen,
     label: "Kill Switches",
-    tooltip: "Kill Switches",
   },
   {
     href: "/settings",
     icon: Settings,
     label: "Settings",
-    tooltip: "Settings",
   },
   {
     href: "/bugreport",
     icon: Bug,
     label: "Bug Report",
-    tooltip: "Bug Report",
   },
 ];
 
 export default function SideBar() {
+  const { pathname } = useRouter();
+  const theme = useMantineTheme();
+
+  console.log(pathname);
   return (
     <M_NavBar width={{ base: "15rem" }} p="md">
       <M_NavBar.Section grow>
-        {navItems.map(({ label, href, icon, tooltip }) => (
-          <NavBarItem label={label} icon={icon} tooltip={tooltip} href={href} />
+        {navItems.map(({ label, href, icon: Icon }) => (
+          <NavLink
+            key={label}
+            label={label}
+            component={Link}
+            href={href}
+            color={theme.colorScheme === "dark" ? "gray" : "blue"}
+            active={pathname === href}
+            icon={<Icon size="1.5rem" strokeWidth={1} />}
+          />
         ))}
       </M_NavBar.Section>
       <M_NavBar.Section>User Information</M_NavBar.Section>
