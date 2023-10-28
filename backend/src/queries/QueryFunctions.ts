26c2c26d2f5d346b114474a86a07fac6cc2e564e
IzzyBeraja <izzyberaja+github@gmail.com>
2023-10-28 14:37:45 -0700

    Removing unused file
    Description:
    - Removing unused QueryFunctions file

--- backend/src/queries/QueryFunctions.ts
+++
@@ -1,5 +0,0 @@
-// Exclude keys from user
-export function exclude<User, Key extends keyof User>(user: User, keys: Key[]): Omit<User, Key> {
-  keys.forEach(key => delete user[key]);
-  return user;
-}
